75f620355cfbe068a3639cbe85549605bca092a0
jprolejko <jprolejko@gmail.com>
2020-04-17 17:35:20 +0200

    KMeans implemented

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # BigDataClustering
-First big task for Big Data course at MIMUW. Comparison of different methods and ways of clustering the data (human proteins depending on their amino-acids).
+First big task for Big Data course at MIMUW. 
+Comparison of different methods and ways of clustering the data (human proteins depending on their amino-acids).
 
 Dataset in repository comes from: https://www.uniprot.org/uniprot/?query=proteome:UP000005640
+
+## Assumptions: 
+   * Shingle length is set to 3. The reason is the fact that dataset contains many genes which names length is 3.
+   * Sequences are converted to sparse vectors because of their format. Vectors size is very big but majority of cells is set to 0. Sparse vectors are much more efficient in such cases.
+   * Task compares ways of converting sequences to 0/1 for each shingle and shingle counts for each shingle.
+   * There is comparison of two distance measures - Euclidian and cosine (only for K-Means since other methods does not support this type of distance measure).
+   * Also, three clustering algorithms available in `mllib` are used: K-Means, Gaussian mixture and Power iteration clustering.
+   * Every clustering method is verified using comparison of computed costs of clusters and the best one is picked.
--- src/main/scala/BigDataClustering.scala
+++ src/main/scala/BigDataClustering.scala
@@ -1,14 +1,15 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.SparkContext._
+
 import org.apache.spark.sql._
-import org.apache.spark.rdd.RDD
 
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
 import org.apache.spark.mllib.clustering.KMeans
-import org.apache.spark.ml.evaluation.Evaluator
+
 
 object BigDataClustering {
-  def makeShingles(string: String, shingle_length: Int) : List[String] = {
+  def makeShingles(string: String, shingle_length: Int): List[String] = {
     var shingles = List[String]()
     if (string == null || string.isEmpty) {
       // nothing
@@ -18,44 +19,51 @@ object BigDataClustering {
     }
     else {
       for (i <- 0 to (string.length() - shingle_length)) {
-        shingles = shingles :+ string.substring(i, i + shingle_length)
+        shingles = string.substring(i, i + shingle_length) +: shingles
       }
     }
 
-    shingles
+    return shingles
   }
 
-  def makeDenseVectors(string: String, shingles: Array[String],
-                       shingle_length: Int): Vector = {
+  def makeSparseVectorsCount(string: String, shingles: Array[String],
+                             shingle_length: Int): Vector = {
     val shingles_in = makeShingles(string, shingle_length)
-    var ret_arr = Array.ofDim[Double](shingles.size)
+    var ret_arr = Array[Int]()
+    var ret_arr_2 = Array[Double]()
 
     var index = 0
     for (shingle <- shingles) {
-      if (shingles_in contains shingle) {
-        ret_arr(index) = 1
-      }
-      else {
-        ret_arr(index) = 0
+      var exists = false
+      for (input_shingle <- shingles_in) {
+        if (input_shingle == shingle) {
+          if (!exists) {
+            ret_arr = ret_arr :+ index
+            ret_arr_2 = ret_arr_2 :+ 1.0
+            exists = true
+          }
+          else {
+            val new_val = ret_arr_2.last + 1.0
+            ret_arr_2 = ret_arr_2.dropRight(1) :+ (ret_arr_2.last + 1.0)
+          }
+        }
       }
 
       index = index + 1
     }
 
-    Vectors.dense(ret_arr)
+    return Vectors.sparse(shingles.length, ret_arr, ret_arr_2)
   }
 
-  def makeSparseVectors(string: String, shingles: Array[String],
-                        shingle_length: Int): Vector = {
-    var shingles_in = makeShingles(string, shingle_length)
+  def makeSparseVectors01(string: String, shingles: Array[String],
+                          shingle_length: Int): Vector = {
+    val shingles_in = makeShingles(string, shingle_length)
     var ret_arr = Array[Int]()
     var ret_arr_2 = Array[Double]()
 
     var index = 0
-    for (shingle <- shingles)
-    {
-      if (shingles_in contains shingle)
-      {
+    for (shingle <- shingles) {
+      if (shingles_in contains shingle) {
         ret_arr = ret_arr :+ index
         ret_arr_2 = ret_arr_2 :+ 1.0
       }
@@ -63,11 +71,35 @@ object BigDataClustering {
       index = index + 1
     }
 
-    return Vectors.sparse(shingles.size, ret_arr, ret_arr_2)
+    return Vectors.sparse(shingles.length, ret_arr, ret_arr_2)
+  }
+
+  def applyKMeans(dataset: Dataset[Vector], is_cosine: Boolean): Array[Double] = {
+    var ret_arr = Array[Double]()
+
+    for (k <- 2 to 10) {
+      val model = new KMeans()
+        .setK(k)
+        .setMaxIterations(3)
+        .setDistanceMeasure(if (is_cosine) "cosine" else "euclidean")
+      val clusters = model.run(dataset.rdd)
+
+      val cost = clusters.computeCost(dataset.rdd)
+      ret_arr = ret_arr :+ cost
+    }
+
+    return ret_arr
+  }
+
+  def applyGaussian(dataset: Dataset[Vector]): Array[Double] = {
+    return Array[Double]()
+  }
+
+  def applyPIC(dataset: Dataset[Vector]): Array[Double] = {
+    return Array[Double]()
   }
 
   implicit val vectorEncoder: Encoder[Vector] = org.apache.spark.sql.Encoders.kryo[Vector]
-  implicit val stringEncoder: Encoder[String] = org.apache.spark.sql.Encoders.kryo[String]
 
   def main(args: Array[String]) {
     val spark = SparkSession.builder
@@ -75,36 +107,45 @@ object BigDataClustering {
       .config("spark.master", "local") // TODO NEEDED TO CHANGE!
       .getOrCreate()
 
+    import spark.implicits._
+
     val inFile = spark.read.format("csv")
       .option("sep", ",")
       .option("inferSchema", "true")
       .option("header", "true")
       .load("file:///home/julex/BigDataClustering/proteins_dataset_sample.csv")
-      .cache
+      .na.drop().cache
 
     val shingling_3 = spark.sparkContext
-      .broadcast(inFile.flatMap(row => makeShingles(row.getString(4), 3))
-      .distinct().collect())
+      .broadcast(inFile.flatMap(row => makeShingles(row.getString(7), 3))
+        .distinct().collect())
 
     val converted_to_sparse_vectors_3 = inFile
-      .map(row => makeSparseVectors(row.getString(4), shingling_3.value, 3))
+      .map(row => makeSparseVectors01(row.getString(7), shingling_3.value, 3)).cache
+    val converted_to_sparse_vectors_3_count = inFile
+      .map(row => makeSparseVectorsCount(row.getString(7), shingling_3.value, 3)).cache
 
-    val kmeans = KMeans.train(converted_to_sparse_vectors_3.rdd, 3, 10)
+    converted_to_sparse_vectors_3.collect().foreach(i => println(i))
 
-    val squered_dist_cost = kmeans.computeCost(converted_to_sparse_vectors_3.rdd)
-    println(squered_dist_cost)
+    println("Computing k-means (euclidean) costs for 0/1 shingles:")
+    val k_means_cost = applyKMeans(converted_to_sparse_vectors_3, is_cosine = false)
+    println("K-means (euclidean) costs for 0/1 shingles:")
+    k_means_cost.foreach(i => println(i))
 
-    // # Make predictions
-    //val predictions = kmeans.transform(converted_to_sparse_vectors_3.rdd)
-
-    // # Evaluate clustering by computing Silhouette score
-    // evaluator = ClusteringEvaluator()\
-    //             .setFeaturesCol("features")\
-    //             .setPredictionCol("prediction")\
-    //             .setMetricName("silhouette")
-    // silhouette_cost[k] = evaluator.evaluate(predictions)
-
-    // converted_to_sparse_vectors_3.collect().foreach(i => println(i))
+    println("Computing k-means (euclidean) costs for counted shingles:")
+    val count_k_means_cost = applyKMeans(converted_to_sparse_vectors_3_count, is_cosine = false)
+    println("K-means (euclidean) costs for counted shingles:")
+    count_k_means_cost.foreach(i => println(i))
+//
+//    val gaussian_cost = applyGaussian(converted_to_sparse_vectors_3)
+//    val count_gaussian_cost = applyGaussian(converted_to_sparse_vectors_3_count)
+//
+//    val pic_cost = applyPIC(converted_to_sparse_vectors_3)
+//    val count_pic_cost = applyPIC(converted_to_sparse_vectors_3_count)
+//
+//    val k_means_cosine_cost = applyKMeans(converted_to_sparse_vectors_3, is_cosine = true)
+//    val count_k_means_cosine_cost = applyKMeans(converted_to_sparse_vectors_3_count, is_cosine = true)
+//
 
     spark.stop()
   }
